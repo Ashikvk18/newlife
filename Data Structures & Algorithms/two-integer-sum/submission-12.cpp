0e0a9292ee3931d7d6a16704921eb45ab63c135e
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-05 20:06:54 -0500

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>map;
+        for(int i = 0; i < nums.size(); i++){
+            int current = nums[i];
+            int compliment =target-current;
+            if(map.contains(compliment)){
+                return {map[compliment],i};
+            }
+            else{
+                map[current] = i;
+            }
+        return {};
+    }
+};
