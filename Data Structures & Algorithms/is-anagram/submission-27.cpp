13b7ebccc7c9656160a78d56fe41abf3e56631dc
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-09 09:16:08 -0500

    Add: is-anagram - submission-27

---
+++ Data Structures & Algorithms/is-anagram/submission-27.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> map;
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(auto x : s){
+            map[x]++;
+        }
+         for(auto x : t){
+            map[x]--;
+        }
+        for(auto x : map){
+            if (x.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
