4650c07296aac649ce60973cb31e196101af34a5
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-09 13:01:09 -0500

    Add: valid-sudoku - submission-38

---
+++ Data Structures & Algorithms/valid-sudoku/submission-38.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<set<char, int>> r(9), c(9), b(9);
+        for(auto i{0uz}; i < 9; i++){
+            for(auto j{0uz}; j < 0; j++){
+                char ch = board[i][j];
+                int x = (i/3)*3 + (j/3);
+                if(ch != '.'){
+                    if(r[i].contains(ch)||c[j].contains(ch)||b[x].contains(ch)){
+                        return false;
+                    }
+                    else{
+                        r[i].insert(ch);
+                        c[j].insert(ch);
+                        b[x].insert(ch);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+ };
