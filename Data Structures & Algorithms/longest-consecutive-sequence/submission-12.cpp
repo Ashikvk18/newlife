cfa1513de8b35134390520b20dc6c7ede3815107
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-09 19:10:27 -0500

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        int maxR = 0;
+        
+        for(auto& y : nums){
+            if(!s.contains(y-1)){
+                int current = y;
+                int streak = 1;
+                while(s.contains(current + 1)){
+                    current++;
+                    streak++;
+                }
+                maxR = max(maxR, streak);
+            }
+        }
+        return maxR;
+    }
+};
