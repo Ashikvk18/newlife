b055ee89a20940241143361b260e45dfbdd33673
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-31 19:48:26 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i {0uz};
+        int j = s.length()-1;
+        while(i<j){
+            if(s[i]<s[j]){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
