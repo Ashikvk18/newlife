c17d3ab9dbcdb6a2ca0abe1e49a434cb4aa31a73
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-06 07:42:11 -0500

    Add: is-palindrome - submission-30

---
+++ Data Structures & Algorithms/is-palindrome/submission-30.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s)
+     {
+        int i {0uz};
+        int j = s.size()-1;
+        while(i<j){
+            while(i<j && !alphaNum(s[i])) //checking github
+            {
+                i++;
+            }
+            while(j>i && !alphaNum(s[j])){
+                j--;
+            }
+            if(tolower(s[i])!=tolower(s[j])){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool alphaNum(char c){
+        return ('A'<=c && c<='Z')||('a'<=c && c<='z')||('0'<=c && c<='9');
+    }
+};
