ad99061808fc8dafd9cc771a43dc1d204843b0fc
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-06 16:15:09 -0500

    Add: duplicate-integer - submission-127

---
+++ Data Structures & Algorithms/duplicate-integer/submission-127.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int>hset;
+        for(auto& x : nums){
+            if(hset.contains(x)){
+                return true;
+            }
+            else{
+               hset.insert(x) ;
+            }
+            return false;
+        }
+    }
+};
